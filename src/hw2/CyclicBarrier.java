0bd19731d947f78700660a50ba240db265a172d0
ryantemplin <ryantemplin@wireless-10-145-37-188.public.utexas.edu>
2015-02-10 17:08:04 -0600

    added Cyclic Barrier Code. Now just needs to be Tested

--- src/hw2/CyclicBarrier.java
+++ src/hw2/CyclicBarrier.java
@@ -1,9 +1,22 @@
 package hw2;
 
 public class CyclicBarrier {
+	int value;
 	
 	public CyclicBarrier(int parties){
-		
+		value = parties;
+	}
+	
+	public synchronized void P(){
+		value--;
+		if (value < 0) 
+			Util.myWait(this);
+	}
+	
+	public synchronized void V(){
+		value++;
+		if (value <= 0)
+			notify();
 	}
 	
 	int await() throws InterruptedException{
