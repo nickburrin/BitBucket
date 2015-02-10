4d5902927ad0a8f0b536a4f4b7f4aa5b905bbc98
Nick <nickburrin@utexas.edu>
2015-02-10 15:13:16 -0600

    Started on dig and plant

--- src/hw2/Garden.java
+++ src/hw2/Garden.java
@@ -1,24 +1,64 @@
 package hw2;
 
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.*;
 
 public class Garden {
+	
 	static int MAX;
-	ReentrantLock shovel = new ReentrantLock();
+	static int empty;
+	static int planted;
+	static int filled;
+	
+	final ReentrantLock shovel = new ReentrantLock();
+	final Condition maxEmpty = shovel.newCondition();
+	final Condition noneEmpty = shovel.newCondition();
+	final Condition waitingToPlant = shovel.newCondition();
 	
 	public Garden(int max){
 		this.MAX = max;
+		this.empty = 0;
+		this.planted = 0; 
+		this.filled = 0; 
 	}
 	
-	public void startDigging(){
-		System.out.println("This is a test");
-	}
-	
-	public void doneDigging(){
+	/**
+	 * Newton digs the holes. He will only dig a new hole if: empty <= MAX.
+	 * He must first grab the shovel.
+	 */
+	public void startDigging() throws InterruptedException{
 		
+		shovel.lock();
+		
+		try{
+			while(empty >= MAX){
+				maxEmpty.await();
+			}	
+			//empty += 1;
+			
+		} catch(InterruptedException e) {}
 	}
 	
+	/**
+	 * Newton is done digging. Puts back the shovel and wakes any threads waiting to plant
+	 */
+	public void doneDigging(){
+		shovel.unlock();
+		waitingToPlant.signal();
+	}
+	
+	/**
+	 * Benjamin will plant a seed if: empty > 0
+	 */
 	public void startSeeding(){
+		
+		try{
+			while(empty < 1){
+				waitingToPlant.await();	
+			}
+			
+			//empty -= 1;
+			//planted += 1;
+		} catch(InterruptedException e){}
 		
 	}
 	
